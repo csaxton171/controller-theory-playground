8264e8d69add2954ca71e0398baa5e4e2f4e32c2
Craig Saxton <craig.saxton@news.co.uk>
2019-12-26 11:08:05 +0000

    feat: add dead zone support

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -9,7 +9,8 @@ import {
 } from "../worker";
 import {
     proportionalControllerFactory,
-    compositeControllerFactory
+    compositeControllerFactory,
+    deadZoneDecorator
 } from "../controller";
 import { ConsoleLogger } from "../Logger";
 
@@ -17,34 +18,51 @@ type ControllerRunConfig = {
     setPoint: number;
     gain: number;
     iterations: number;
+    deadZone: [number, number] | [];
     debug: boolean;
 };
 
 export const describe = "runs controller against plant";
 
 export const builder = () =>
-    yargs.options({
-        iterations: {
-            describe: "number of iterations/time-units to execute",
-            type: "number",
-            default: 120
-        },
-        setPoint: {
-            describe: "desired plant output signal value",
-            type: "number",
-            require: true
-        },
-        gain: {
-            describe: "gain applied to controller signal output",
-            type: "number",
-            default: 1
-        },
-        debug: {
-            describe: "output worker plant activity",
-            type: "boolean",
-            default: false
-        }
-    });
+    yargs
+        .options({
+            iterations: {
+                describe: "number of iterations/time-units to execute",
+                type: "number",
+                default: 120
+            },
+            setPoint: {
+                alias: "sp",
+                describe: "desired plant output signal value",
+                type: "number",
+                require: true
+            },
+            gain: {
+                alias: "g",
+                describe: "gain applied to controller signal output",
+                type: "number",
+                default: 1
+            },
+            debug: {
+                describe: "output worker plant activity",
+                type: "boolean",
+                default: false
+            },
+            deadZone: {
+                alias: "dz",
+                describe: "an input range expressed as 'min-max' ",
+                type: "string",
+                default: ""
+            }
+        })
+        .coerce({
+            deadZone: (opt: string) =>
+                !opt.trim() ? [] : opt.split("-").map(e => parseInt(e))
+        })
+        .usage(
+            "$0 run --set-point <number> [--gain <number>] [--terations <number>]"
+        );
 
 export const handler = async (argv: ControllerRunConfig) => {
     const workerSpecs: WorkDuration[] = [10, 5, 1, 5, 15, 30, 15];
@@ -54,31 +72,43 @@ export const handler = async (argv: ControllerRunConfig) => {
     for (const result of baselinePlant.iterate(argv.iterations)) {
         baselineSeries.push(result.length);
     }
+    outputConfig(argv, workerSpecs);
 
-    const controllers = compositeControllerFactory([
+    let controller = compositeControllerFactory([
         proportionalControllerFactory(argv.setPoint, argv.gain)
     ]);
+
+    if (argv.deadZone.length == 2) {
+        controller = deadZoneDecorator(controller, argv.deadZone);
+    }
     const controlledPlant = new WorkerPlant(makeWorkers(workerSpecs), logger);
     const controlledSeries = [];
     for (const result of controlledPlant.iterate(argv.iterations)) {
         controlledSeries.push(result.length);
 
         controlledPlant.addWorkers(
-            makeCountWorkers(controllers(result.length), randomDuration())
+            makeCountWorkers(controller(result.length), randomDuration())
         );
     }
 
-    console.table([
-        { param: "initial-workers", value: workerSpecs.length },
-        { param: "iterations", value: argv.iterations },
-        { param: "set-point", value: argv.setPoint },
-        { param: "gain", value: argv.gain }
-    ]);
     console.log("[ baseline plant (uncontrolled) ]");
     console.log(outputPlantSeries(baselineSeries));
     console.log("\n\n");
     console.log("[ subject plant ]");
     console.log(outputPlantSeries(controlledSeries));
+};
+
+const outputConfig = (
+    argv: ControllerRunConfig,
+    workerSpecs: WorkDuration[]
+) => {
+    console.table([
+        { param: "initial-workers", value: workerSpecs.length },
+        { param: "iterations", value: argv.iterations },
+        { param: "set-point", value: argv.setPoint },
+        { param: "gain", value: argv.gain },
+        { param: "dead-zone", value: argv.deadZone }
+    ]);
 };
 
 const outputPlantSeries = (series: number[]) => plot(series, { height: 10 });
