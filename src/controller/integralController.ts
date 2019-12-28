5cf969fbbc5bcf915161bcfdbaddca07c6feb5e8
Craig Saxton <craig.saxton@news.co.uk>
2019-12-28 18:38:14 +0000

    refactor: utilise rambda for integral controller

--- src/controller/integralController.ts
+++ src/controller/integralController.ts
@@ -1,9 +1,10 @@
 import { Controller, calculateError } from "../controller";
+import { sum } from "ramda";
 
 export const integralControllerFactory = (
     setPoint: number,
     errors: number[] = []
 ): Controller => (input: number) => {
     errors.push(calculateError(setPoint, input));
-    return errors.reduce((acc, cur) => acc + cur, 0);
+    return sum(errors);
 };
